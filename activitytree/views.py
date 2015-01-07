fcd333afdaa4d525657cdc69a750aede6e9d27f6
mariosky <mariosky@gmail.com>
2015-01-07 14:08:48 -0800

    protoboard title

--- activitytree/views.py
+++ activitytree/views.py
@@ -58,8 +58,6 @@ def welcome(request):
                 context_instance=RequestContext(request))
 
 def activity(request,uri):
-    print request.user, request.user.is_authenticated()
-
     if request.user.is_authenticated():
         s = SimpleSequencing()
 
@@ -573,57 +571,8 @@ def _check_survey(post_dict, quiz):
 
     return checked
 
-# @sensitive_post_parameters()
-# @csrf_protect
-# @never_cache
-# def login(request, template_name='registration/login.html',
-#           redirect_field_name=REDIRECT_FIELD_NAME, authentication_form=AuthenticationForm,
-#           current_app=None, extra_context=None):
-#     """
-#     Displays the login form and handles the login action.
-#     """
-#     redirect_to = request.REQUEST.get(redirect_field_name, '')
-#
-#     if request.method == "POST":
-#         form = authentication_form(request, data=request.POST)
-#         if form.is_valid():
-#
-#             # Ensure the user-originating redirection url is safe.
-#             if not is_safe_url(url=redirect_to, host=request.get_host()):
-#                 redirect_to = resolve_url(settings.LOGIN_REDIRECT_URL)
-#
-#             # Okay, security check complete. Log the user in.
-#             auth_login(request, form.get_user())
-#
-#             return HttpResponseRedirect(redirect_to)
-#     else:
-#         form = authentication_form(request)
-#
-#
-#
-#     current_site = get_current_site(request)
-#
-#     from django.conf import settings
-#     plus_scope = ' '.join(GooglePlusAuth.DEFAULT_SCOPE)
-#     plus_id=settings.SOCIAL_AUTH_GOOGLE_PLUS_KEY
-#
-#     context = {
-#         'form': form,
-#         redirect_field_name: redirect_to,
-#         'site': current_site,
-#         'site_name': current_site.name,
-#         'plus_scope':plus_scope,'plus_id':plus_id
-#     }
-#     if extra_context is not None:
-#         context.update(extra_context)
-#     return TemplateResponse(request, template_name, context,
-#                             current_app=current_app)
-#
-
 def login_view(request,template_name='registration/login.html',  redirect_field_name=REDIRECT_FIELD_NAME):
     return TemplateResponse(request, template_name, current_app=None)
-
-
 
 
 
@@ -654,8 +603,6 @@ def facebook_get_login(request):
     return HttpResponseRedirect(url)
 
 def facebook_login(request):
-
-
     if 'error' in request.GET:
         return HttpResponseRedirect('/')
 
