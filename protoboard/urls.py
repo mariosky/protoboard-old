1a6e5856b0ef9816df3aecf5008ec40ca878d492
mariosky <mariosky@gmail.com>
2015-03-02 14:44:55 -0800

    dashboard

--- protoboard/urls.py
+++ protoboard/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     # Example:
       (r'^$','activitytree.views.welcome'),
       (r'^welcome/$','activitytree.views.welcome'),
+      (r'^dashboard/(?P<uri>\w+)$','activitytree.views.dashboard'),
+
       (r'^activity/(?P<uri>\w+)$','activitytree.views.activity'),
       (r'^activity/video/(?P<uri>\w+)$','activitytree.views.activity'),
       (r'^test/(?P<uri>\w+)$','activitytree.views.test'),
