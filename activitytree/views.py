6e9d4b470c18413ac22bcf40797691bba7aeea68
mariosky <mariosky@gmail.com>
2015-03-04 09:48:06 -0800

    attempts

--- activitytree/views.py
+++ activitytree/views.py
@@ -306,7 +306,7 @@ def test(request, uri, objective_status = None):
 
                 feedback = _check_quiz(request.POST, quiz)
                 # Updates the current Learning Activity
-                objective_measure = float(feedback['total_correct'])/float(quiz['questions'])
+                objective_measure = float(feedback['total_correct'])/len(quiz['questions'])
                 if objective_measure >= activities[requested_activity.learning_activity.uri]['satisfied_at_least']:
                     objective_status='satisfied'
                 else:
