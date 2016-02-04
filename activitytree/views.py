724f04b1f80c5cb32f243d0579971f83a71b414a
Rene <renemarquezvalenzuela@gmail.com>
2016-02-03 19:44:24 -0800

    cambie actividades a base.html para que se puedan hacer desde inicio y en gral desde toda la pagina

--- activitytree/views.py
+++ activitytree/views.py
@@ -138,12 +138,9 @@ def actividad(request,course_id):
     #GET:
     #Edit course
     elif request.method == 'GET':
-        if course_id :
-
-            course = get_activity_tree(course_id)
-            return HttpResponse(json.dumps(course), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound('<h1>Course ID not Found</h1>')
+        return render_to_response('activitytree/activity_builder.html', context_instance=RequestContext(request))
+    else:
+        return HttpResponseNotFound('<h1>Course ID not Found</h1>')
 
 
 
