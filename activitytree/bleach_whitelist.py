2f1e63ce14dece0bcaa66c9eb1d59948ae568d75
Mario Garcia Valdez <mariosky@gmail.com>
2016-09-12 21:10:01 -0700

    pre, row and button elements added to white list

--- activitytree/bleach_whitelist.py
+++ activitytree/bleach_whitelist.py
@@ -2,7 +2,7 @@
 
 all_tags = [
     u'a', u'abbr', u'acronym', u'b', u'blockquote', u'code', u'em', u'i', u'li', u'ol', u'strong', u'ul',
-    u'h1',u'h2',u'h3',u'h4',u'h5',u'p',u'sub',u'div',u'dl',u'dt',u'dd'
+    u'h1',u'h2',u'h3',u'h4',u'h5',u'p',u'sub',u'div',u'dl',u'dt',u'dd',u'pre',u'row',u'button'
 ]
 
 attrs = {
