fbee4a4f076bde4eabd3dd7f066b6cab4891675c
Mario García Valdez <mariosky@gmail.com>
2018-09-29 14:51:27 -0700

    NEW WSGI FILE

--- protoboard/wsgi.py
+++ protoboard/wsgi.py
@@ -1,14 +1,17 @@
+
 """
-WSGI config for protoboard project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "protoboard.settings")
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'protoboard.settings')
+
 application = get_wsgi_application()
